bc4f48d008dc1c7dce0e104cec1383b6398f7a7a
inesms7 <43278764+inesms7@users.noreply.github.com>
2020-02-25 16:09:32 +0000

    Display Current Location Temperature

--- src/script.js
+++ src/script.js
@@ -42,17 +42,33 @@ function showTemp(response) {
 	);
 }
 
+function searchLocation(position) {
+	let lat = position.coords.latitude;
+	let lon = position.coords.longitude;
+	let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+	axios.get(url).then(showTemp);
+}
+
+function search(city) {
+	let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+	axios.get(weatherUrl).then(showTemp);
+}
+
 function searchCity(event) {
 	event.preventDefault();
 	let city = document.querySelector("#city").value;
-	let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-	axios.get(weatherUrl).then(showTemp);
+	search(city);
+}
+
+function getLocation(event) {
+	event.preventDefault();
+	navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 let searchForm = document.querySelector("#form");
 searchForm.addEventListener("submit", searchCity);
 
-//current location id  #location  | temperature #temp | #city | #form
+search("Rome");
 
-//let currentLocationButton = document.querySelector("#location");
-//zcurrentLocationButton.addEventListener("click", getLocation);
+let currentLocationButton = document.querySelector("#location");
+currentLocationButton.addEventListener("click", getLocation);
